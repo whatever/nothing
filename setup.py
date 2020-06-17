c4211ec10c8ac4099f58f809b44988178729ba42
whatever <mattisbusiness@gmail.com>
2020-06-17 17:42:22 -0400

    :hocho:

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-maddog", # Replace with your own username
+    version="0.0.1",
+    author="Matt Derasoirs",
+    author_email="nothing@razorbla.de",
+    scripts=["bin/whatever-okay"],
+    description="package about nothing",
+    url="http://razorbla.de",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
